80337e28350864cab67b7b5fc92fe8ca0f8ecae8
martinocrippa <30796289+martinocrippa@users.noreply.github.com>
2023-09-12 18:23:51 +0200

    Update streamlit_app.py
    insert try and except to controll api call and change call to api

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,18 +28,22 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to diplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-# get data from api
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
- # streamlit.text(fruityvice_response.json())  # just write data to screen
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   if not fruit_choice:
+       streamlit.error('Please select a fruit to get inormation.')
+    else:
+       # get data from api
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+       # streamlit.text(fruityvice_response.json())  # just write data to screen
+       # write your own comment -what does the next line do? 
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   
+       # write your own comment - what does this do?
+       streamlit.dataframe(fruityvice_normalized)
+     
+except URLError as e:
+    streamlit.error()
 
 # don0t run anything past here while we troibleshoot
 streamlit.stop()

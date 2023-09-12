965aac5edba52f0a182e2aa7ea32d252c5db0986
martinocrippa <30796289+martinocrippa@users.noreply.github.com>
2023-09-12 17:52:14 +0200

    Update streamlit_app.py
    add text box entry

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,7 @@ my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# New Section add a second text box entry
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)

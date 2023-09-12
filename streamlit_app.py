274731e403460b272e5f2e24c05eed970b658608
martinocrippa <30796289+martinocrippa@users.noreply.github.com>
2023-09-12 18:24:19 +0200

    Update streamlit_app.py
    correct typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
    if not fruit_choice:
        streamlit.error('Please select a fruit to get inormation.')
-    else:
+   else:
        # get data from api
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
        # streamlit.text(fruityvice_response.json())  # just write data to screen

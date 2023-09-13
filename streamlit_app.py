221a101f8168fa1c43d96f5ad4666d54c0cb6abf
martinocrippa <30796289+martinocrippa@users.noreply.github.com>
2023-09-13 08:42:48 +0200

    Update streamlit_app.py
    generalization api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# create the repeatable code blocck (called a function)
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
 # New Section to diplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -34,12 +39,7 @@ try:
        streamlit.error('Please select a fruit to get inormation.')
    else:
        # get data from api
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-       # streamlit.text(fruityvice_response.json())  # just write data to screen
-       # write your own comment -what does the next line do? 
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   
-       # write your own comment - what does this do?
+       back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dataframe(fruityvice_normalized)
      
 except URLError as e:
